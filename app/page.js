707cb30ad64d458c6dc703b17700492a73b3b482
danieldaudu65 <danieldaudu65@gmail.com>
2024-08-05 15:48:50 +0100

    'build'

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
 
+'use client'
+import BlogItem from "@/components/BlogItem";
+import Bloglist from "@/components/Bloglist";
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
     <>
-    
+      <Header />
+      <Bloglist />
     </>
   );
 }
